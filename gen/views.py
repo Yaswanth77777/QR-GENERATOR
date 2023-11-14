90b9b86fe04dd8c18a168f06b140b91f8446439d
Yaswanth77777 <132152566+Yaswanth77777@users.noreply.github.com>
2023-11-14 19:25:33 +0530

    Update views.py

--- gen/views.py
+++ gen/views.py
@@ -11,12 +11,12 @@ def index(request):
         if request.method =="POST":
             data = request.POST['link-data']
             img= make(data)
-           buffer = BytesIO()
-        img.save(buffer, format='PNG')
-        qr_bytes = buffer.getvalue()
+            buffer = BytesIO()
+            img.save(buffer, format='PNG')
+            qr_bytes = buffer.getvalue()
 
        
-        qr_base64 = base64.b64encode(qr_bytes).decode('utf-8')
+            qr_base64 = base64.b64encode(qr_bytes).decode('utf-8')
             return render(request,'index.html',{'generated_img':qr_base64})
 
         return render(request,'index.html')
